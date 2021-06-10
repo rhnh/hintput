32fa93050e0493cb95a412d6a85581dffebf1726
rhnh <nrhbd@protonmail.com>
2021-06-10 22:43:14 +0200

    publishing with types not working

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import external from "rollup-plugin-peer-deps-external";
 import babel from "rollup-plugin-babel";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 export default [
   {
     input: "./src/index.tsx",
@@ -28,6 +29,7 @@ export default [
       postcss({
         plugins: [],
       }),
+      terser(),
     ],
   },
 ];
